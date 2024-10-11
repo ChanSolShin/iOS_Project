ca1780836645b326a4f204e09e56449723952630
Shin Chan Sol <leeh4743@naver.com>
2024-10-11 18:39:17 +0900

    MeetingListView 생성

---
+++ iOS_Project/MeetingListView.swift
@@ -0,0 +1,23 @@
+//
+//  MeetingListView.swift
+//  iOS_Project
+//
+//  Created by 신찬솔 on 10/11/24.
+//
+
+import SwiftUI
+
+struct MeetingListView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+// 프리뷰용 코드
+struct MeetingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingListView()
+    }
+}
+
+

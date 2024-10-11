734e52e51270d9c3d032957a928d3f2fbd1a5051
Shin Chan Sol <leeh4743@naver.com>
2024-10-11 19:09:47 +0900

    docs(change): id TextField change
    id TextField의 텍스트를 아이디에서 이메일로 변경

--- iOS_Project/LoginView.swift
+++ iOS_Project/LoginView.swift
@@ -15,15 +15,16 @@ struct LoginView: View {
         NavigationView {
             VStack {
                 // 앱 이름 표시
-                Text("MyApp")
+                Text("MyApp") // 이곳에 앱 이름 적기
                     .font(.largeTitle)
-                    .padding(.bottom, 40)
+                    .padding(.top, 70)
+                    .padding(.bottom, 70)
                 
-                // 아이디 입력 필드 (유저 이미지 포함)
+                // 아이디 입력 필드
                 HStack {
-                    Image(systemName: "person") // 유저 이미지
+                    Image(systemName: "person") 
                         .foregroundColor(.gray)
-                    TextField("아이디", text: $username)
+                    TextField("이메일", text: $username)
                         .autocapitalization(.none)
                 }
                 .padding()
@@ -31,9 +32,9 @@ struct LoginView: View {
                 .cornerRadius(8)
                 .padding(.horizontal, 40)
 
-                // 비밀번호 입력 필드 (자물쇠 이미지 포함)
+                // 비밀번호 입력 필드
                 HStack {
-                    Image(systemName: "lock") // 자물쇠 이미지
+                    Image(systemName: "lock")
                         .foregroundColor(.gray)
                     SecureField("비밀번호", text: $password)
                 }
@@ -44,24 +45,20 @@ struct LoginView: View {
                 .padding(.bottom, 40)
                 
                 // 로그인 및 회원가입 버튼
-                HStack(spacing: 30) { // 두 버튼 사이 간격을 30으로 설정
-                    // 로그인 버튼: MeetingListView로 이동
+                HStack(spacing: 80) {
                     NavigationLink(destination: MeetingListView()) {
                         Text("로그인")
                             .foregroundColor(.blue)
                     }
 
-                    Button(action: {
-                        // 회원가입 액션
-                    }) {
+                    NavigationLink(destination: CreateAccountView())  {
                         Text("회원가입")
                             .foregroundColor(.blue)
                     }
                 }
                 .padding(.horizontal, 40)
                 
-                Spacer() // 화면 하단 여백
-            }
+                Spacer()             }
         }
     }
 }

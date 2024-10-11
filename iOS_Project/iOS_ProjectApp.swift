d52fe17da98f075efa839f7fee4122fa84f4e9f8
Shin Chan Sol <leeh4743@naver.com>
2024-10-11 18:07:15 +0900

    Initial Commit

---
+++ iOS_Project/iOS_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ProjectApp.swift
+//  iOS_Project
+//
+//  Created by 신찬솔 on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS_ProjectUITests/iOS_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_ProjectUITestsLaunchTests.swift
+//  iOS_ProjectUITests
+//
+//  Created by 신찬솔 on 10/11/24.
+//
+
+import XCTest
+
+final class iOS_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
